c874847f9682c405c6957142d1561d252ac19591
EliseGabilly <elise.gabilly@gmail.com>
2022-03-18 11:18:13 +0100

    Setup

---
+++ manip/src/main/java/com/example/manip/ManipApplication.java
@@ -0,0 +1,13 @@
+package com.example.manip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManipApplication.class, args);
+	}
+
+}
