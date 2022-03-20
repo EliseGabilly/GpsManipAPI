d720dc619e4e818568e4e2cbf9224df89092fa77
EliseGabilly <elise.gabilly@gmail.com>
2022-03-20 10:27:17 +0100

    Link to mysql...

--- manip/src/main/java/com/example/manip/ManipApplication.java
+++ manip/src/main/java/com/example/manip/ManipApplication.java
@@ -14,14 +14,14 @@ public class ManipApplication {
 		SpringApplication.run(ManipApplication.class, args);
 	}
 
-	@Bean
-	CommandLineRunner init(CoordinateRepository coordinateRepository) {
-		return args -> {
-			Stream.of(48.8d, 48.81d, 48.85d, 48.9d, 50d, 42d).forEach(lat -> {
-				Coordinates coordinates = new Coordinates(lat, 2.28d);
-				coordinateRepository.save(coordinates);
-			});
-			coordinateRepository.findAll().forEach(System.out::println);
-		};
-	}
+//	@Bean
+//	CommandLineRunner init(CoordinateRepository coordinateRepository) {
+//		return args -> {
+//			Stream.of(48.8d, 48.81d, 48.85d, 48.9d, 50d, 42d).forEach(lat -> {
+//				Coordinates coordinates = new Coordinates(lat, 2.28d);
+//				coordinateRepository.save(coordinates);
+//			});
+//			coordinateRepository.findAll().forEach(System.out::println);
+//		};
+//	}
 }
